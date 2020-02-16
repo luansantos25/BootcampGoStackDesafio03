6e19bb2c088fe5c9384474769aabc56df595ab0d
Luan <luan.dev.santos@gmail.com>
2020-02-16 18:10:29 -0300

    feat: adding file uploads system with database association

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
 import DelivererController from './app/controllers/DelivererController';
+import FileController from './app/controllers/FileController';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -14,6 +18,8 @@ routes.post('/users', UserController.store);
 
 routes.use(authMiddlewares);
 
+const upload = multer(multerConfig);
+
 routes.put('/users', UserController.update);
 
 routes.post('/recipients', RecipientController.store);
@@ -23,5 +29,7 @@ routes.get('/couriers', DelivererController.index);
 routes.post('/couriers', DelivererController.store);
 routes.put('/couriers/:id', DelivererController.update);
 routes.delete('/couriers/:id', DelivererController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
